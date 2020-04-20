679709df13ca5b46f5616f382b92ada90bf99117
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-20 19:14:39 +0500

    Completed Lesson 5

--- Aspcore/EmployeeManagement/Program.cs
+++ Aspcore/EmployeeManagement/Program.cs
@@ -6,18 +6,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+C:\Users\Najeeb Tiwana\source\repos\Aspcore\Aspcore\EmployeeManagement\appsettings.json
 namespace EmployeeManagement
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            //in aspnet core file and folder references are not included
-            //target framwork monika is 3.1
-            //hosting model:iprocess,out process
-            //meta package has no package of its own it just contain dependecies
-            //
+            //significance of main method
             CreateHostBuilder(args).Build().Run();
         }
 
