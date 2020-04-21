7c32ad4e8a154caf6d7d395d86229646849208cc
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-21 09:32:27 +0500

    Completed Lesson 9

--- Aspcore/EmployeeManagement/Startup.cs
+++ Aspcore/EmployeeManagement/Startup.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,6 +15,12 @@ namespace EmployeeManagement
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -36,7 +45,7 @@ namespace EmployeeManagement
               {
                   _ = endpoints.MapGet("/", async context =>
                     {
-                        await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+                        await context.Response.WriteAsync(_config["MyKey"]);
                     });
               });
         }
