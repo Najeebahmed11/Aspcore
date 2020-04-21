95e969dad5ffe046d6e562173ecd3cb4bbc43db4
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-21 20:52:31 +0500

    Completed Lesson 17

---
+++ Aspcore/EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeManagement.Models;
+using System;
+using System.Collections.Generic;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>
+                {
+                    new Employee(){Id=1,Name="najeeb",Department="cs"},
+                    new Employee() { Id = 2, Name = "Hinan", Department = "cs" }
+                };
+        }
+        public Employee GetEmployee(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
