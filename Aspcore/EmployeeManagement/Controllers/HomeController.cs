1e44e9d614a24762d344793b2bc8e5a94fe4d7b7
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-04-27 22:26:42 +0500

    Completed Lesson 26

--- Aspcore/EmployeeManagement/Controllers/HomeController.cs
+++ Aspcore/EmployeeManagement/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using DocumentFormat.OpenXml.Office2013.Drawing.ChartStyle;
 using DocumentFormat.OpenXml.Wordprocessing;
 using EmployeeManagement.Models;
+using EmployeeManagement.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.ViewFeatures.Buffers;
@@ -25,10 +26,13 @@ namespace EmployeeManagement.Controllers
         }
         public ViewResult Details()
         {
+            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
+            {
+                Employee = _employeeRepository.GetEmployee(1),
+                PageTitle = "EmployeeDetails"
+            };
             //in relative path we do not use extension
-            Employee model = _employeeRepository.GetEmployee(1);
-            ViewBag.PageTittle = "EmployeeDetails";
-            return View(model);
+            return View(homeDetailsViewModel);
         }
 
     }
