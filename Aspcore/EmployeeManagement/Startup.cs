aeed774b3968b1f3b862ee7aee9e503c292d86bb
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-05-04 21:23:09 +0500

    Completed Lesson 36 Why use tag helpers

--- Aspcore/EmployeeManagement/Startup.cs
+++ Aspcore/EmployeeManagement/Startup.cs
@@ -56,7 +56,7 @@ namespace EmployeeManagement
               app.UseEndpoints(endpoints =>
             {
               endpoints.MapControllerRoute(name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "ivas/{controller=Home}/{action=Index}/{id?}");
                // endpoints.MapControllers();
             });
         }
