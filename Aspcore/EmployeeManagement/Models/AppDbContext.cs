731b44161eee56651dd9e5e48406dc964e4dcea5
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-05-05 17:26:08 +0500

    Completed Lesson 47 DbContext in entity framework core

---
+++ Aspcore/EmployeeManagement/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+namespace EmployeeManagement.Models
+{
+    public class AppDbContext: Microsoft.EntityFrameworkCore.DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
