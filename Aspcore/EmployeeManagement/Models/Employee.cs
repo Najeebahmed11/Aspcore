a5add9fdf62c362c26ddf852c51016741a32c8cd
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-05-05 13:47:55 +0500

    Completed Lesson 43 Select list validation in asp net core

--- Aspcore/EmployeeManagement/Models/Employee.cs
+++ Aspcore/EmployeeManagement/Models/Employee.cs
@@ -18,7 +18,8 @@ namespace EmployeeManagement.Models
             ErrorMessage ="invalid email Fprmat")]
         [Display(Name="Office Email")]
         public string Email { get; set; }
-        public Dept Department { get; set; }
+        [Required]
+        public Dept? Department { get; set; }
 
     }
 }
