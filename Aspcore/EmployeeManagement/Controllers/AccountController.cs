4165565c1816ba69d1aef4dedb8c2ad4e48e1121
Najeebahmed11 <43084585+Najeebahmed11@users.noreply.github.com>
2020-05-09 17:15:28 +0500

    Completed Lesson 84 List all users from asp net core identity database

--- Aspcore/EmployeeManagement/Controllers/AccountController.cs
+++ Aspcore/EmployeeManagement/Controllers/AccountController.cs
@@ -67,6 +67,10 @@ namespace EmployeeManagement.Controllers
                 var result = await userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    if (signInManager.IsSignedIn(User) && User.IsInRole("Admin"))
+                    {
+                        return RedirectToAction("ListUsers", "Administration");
+                    }
                     await signInManager.SignInAsync(user, isPersistent: false);
                     return RedirectToAction("index", "home");
                 }
